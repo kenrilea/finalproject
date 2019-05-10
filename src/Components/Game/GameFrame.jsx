1676d41ba318d90b12703472f5593f286934fdc0
Sam S <salinas144@gmail.com>
2019-05-10 17:57:55 -0400

    Basic tile drawing test in SVG

--- src/Components/Game/GameFrame.jsx
+++ src/Components/Game/GameFrame.jsx
@@ -2,15 +2,66 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./../../css/gameFrame.css";
 
+const Sky = () => {
+  const skyStyle = {
+    fill: "#30abef"
+  };
+  const skyWidth = 5000;
+  const gameHeight = 1500;
+  return (
+    <rect
+      style={skyStyle}
+      x={skyWidth / -2}
+      y={100 - gameHeight}
+      width={skyWidth}
+      height={gameHeight}
+    />
+  );
+};
+
+const tiles = () => {
+  const dimensions = { x: 8, y: 8 };
+  const width = 100 / dimensions.x;
+  const height = 100 / dimensions.y;
+  let tiles = [];
+  for (let x = 0; x < 100; x += width) {
+    for (let y = 0; y < 100; y += height) {
+      tiles.push({
+        x,
+        y,
+        width,
+        height
+      });
+    }
+  }
+  console.log("tiles: ", tiles);
+  return tiles;
+};
+
 class GameFrame extends Component {
   render = () => {
     return (
-      <div className="gameframe wrapper">
-        <p>
-          container with fixed aspect ratio of 5/4 that expands to take up as
-          much of the available space as it can
-        </p>
-      </div>
+      <svg
+        className="gameframe wrapper"
+        id="chess-2-canvas"
+        preserveAspectRatio="xMaxYMax none"
+      >
+        {tiles().map(tile => {
+          const style = {
+            fill: "#004400"
+            //transform: "rotate3d(0.6, -0.2, 0.2, 75deg)"
+          };
+          return (
+            <rect
+              style={style}
+              x={tile.x + "%"}
+              y={tile.y + "%"}
+              width={tile.width + "%"}
+              height={tile.height + "%"}
+            />
+          );
+        })}
+      </svg>
     );
   };
 }
