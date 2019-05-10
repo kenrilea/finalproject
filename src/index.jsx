ee71ae1d0852bc46c6b96cde6c1178a7a0dc9988
Sam S <salinas144@gmail.com>
2019-05-10 15:12:04 -0400

    Added divs for layout

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import store from "./store.js";
 import App from "./Components/App.jsx";
+import GameFrame from "./Components/Game/GameFrame.jsx";
 import AnimatedMessage from "./Components/AnimatedMessage.jsx";
 import Oops from "./Components/Oops.jsx";
 
@@ -13,14 +14,19 @@ let root = (
     <BrowserRouter>
       <div className="container">
         <Route exact={false} path="/" component={AnimatedMessage} />
-        <Switch>
-          <Route exact={true} path="/" component={App} />
-          <Route
-            render={props => (
-              <Oops {...props} message={"This page doesn't exist."} />
-            )}
-          />
-        </Switch>
+        <div className="topbar" />
+        <div className="game-chat-container">
+          <Switch>
+            <Route exact={true} path="/" component={App} />
+            <Route exact={true} path="/game/:gameId" component={GameFrame} />
+            <Route
+              render={props => (
+                <Oops {...props} message={"This page doesn't exist."} />
+              )}
+            />
+          </Switch>
+          <div className="friends" />
+        </div>
       </div>
     </BrowserRouter>
   </Provider>
