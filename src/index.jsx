4d7456c52d325ef85e541dd319fecdbaf0effcbf
Sam S <salinas144@gmail.com>
2019-05-10 14:25:31 -0400

    Downgraded react-redux from 7.0.3 to 6.0.1 to fix issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,29 @@
-import ReactDOM from 'react-dom'
-import './main.css'
-import App from './App.jsx'
-import React from 'react'
+import ReactDOM from "react-dom";
+import "./css/main.css";
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import store from "./store.js";
+import App from "./Components/App.jsx";
+import AnimatedMessage from "./Components/AnimatedMessage.jsx";
+import Oops from "./Components/Oops.jsx";
 
+let root = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className="container">
+        <Route exact={false} path="/" component={AnimatedMessage} />
+        <Switch>
+          <Route exact={true} path="/" component={App} />
+          <Route
+            render={props => (
+              <Oops {...props} message={"This page doesn't exist."} />
+            )}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+);
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(root, document.getElementById("root"));
