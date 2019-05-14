5c6fe03e131e75b5af65d72890494eb135bc029d
Kenton <kenrilea@gmail.com>
2019-05-14 17:02:11 -0400

    game now works?

--- testServer.js
+++ testServer.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const multer = require("multer");
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
-const MongoClient = require("mongodb").MongoClient;
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
@@ -17,29 +16,13 @@ const gameData = require(__dirname + "/game-logic/DATA.js");
 
 const upload = multer({ dest: __dirname + "/assets/" }); // Set file upload destination
 const imagePath = "/assets/";
-const url =
-  "mongodb+srv://admin:admin@samurai-murit.mongodb.net/test?retryWrites=true"; // URI for remote database!
-
 app.use("/assets", express.static(__dirname + "/assets"));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //************ STORAGE ************//
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let usersCollection;
-let sessionsCollection;
-let gamesCollection;
-//_______SOCKET STORAGE_____________
-let socketClients = {};
-
 //Connection to DB, do not close!
-MongoClient.connect(url, { useNewUrlParser: true }, (err, allDbs) => {
-  // Add option useNewUrlParser to get rid of console warning message
-  if (err) throw err;
-  finalProjectDB = allDbs.db("FinalProject-DB");
-  usersCollection = finalProjectDB.collection("Users");
-  sessionsCollection = finalProjectDB.collection("Sessions");
-});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //************ GENERAL FUNCTIONS ************//
@@ -54,97 +37,9 @@ const generateId = () => {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //************ SIGNUP ************//
-app.post("/signup", upload.none(), function(req, res) {
-  //Check user collection in remote database to see if username already exists
-  usersCollection
-    .find({ username: req.body.username })
-    .toArray((err, result) => {
-      if (result[0] !== undefined) {
-        // If database return any entry, user exists already!
-        console.log("DB: Be yourself! Try something original...");
-        res.send(JSON.stringify({ success: false }));
-        return;
-      }
-      const newUser = {
-        //userId: generateId(), use _id instead
-        username: req.body.username,
-        password: req.body.password,
-        country: req.body.country,
-        wins: 0,
-        losses: 0,
-        joinedDate: req.body.joinedDate
-      };
-      usersCollection.insertOne(newUser, (err, result) => {
-        //Add new user to remote database
-        if (err) throw err;
-        console.log(
-          `DB: Successfully inserted user ${
-            req.body.username
-          } into Users collection`
-        );
 
-        const newSessionId = generateId();
-        sessionsCollection.insertOne(
-          { sessionId: newSessionId, user: req.body.username },
-          (err, result) => {
-            if (err) throw err;
-            console.log(
-              `DB: Successfully inserted user ${
-                req.body.username
-              } into Users collection`
-            );
-
-            const newSessionId = generateId();
-            sessionsCollection.insertOne(
-              { sessionId: newSessionId, user: req.body.username },
-              (err, result) => {
-                if (err) throw err;
-                console.log(
-                  "DB: Successfully added entry to Sessions collection"
-                );
-                res.cookie("sid", newSessionId);
-                res.send(
-                  JSON.stringify({ success: true, username: req.body.username })
-                );
-              }
-            );
-          }
-        );
-      });
-    });
-});
 //************ LOGIN ************//
-app.post("/login", upload.none(), function(req, res) {
-  const { username: enteredName, password: enteredPass } = req.body;
-  // Check remote users collection in db
-  usersCollection.find({ username: enteredName }).toArray((err, result) => {
-    console.log("DB: Retrieving expected password for user");
-    if (err) throw err;
-    if (result[0] === undefined) {
-      console.log("DB: User not found");
-      res.send(JSON.stringify({ success: false }));
-      return;
-    }
-    const expectedPass = result[0].password;
-    if (enteredPass !== expectedPass) {
-      // Check that password matches
-      console.log("Passwords did not match!");
-      res.send(JSON.stringify({ success: false }));
-      return;
-    }
-    const newSessionId = generateId(); // Generate random number for sid cookie
-    sessionsCollection.insertOne(
-      { sessionId: newSessionId, user: enteredName },
-      (err, result) => {
-        if (err) throw err;
-        console.log("DB: Successfully added entry to Sessions collection");
-      }
-    );
-    console.log(`Logging in user ${enteredName}`);
-    res.cookie("sid", newSessionId); // Send back set-cookie and successful response
-    res.send(JSON.stringify({ success: true, username: enteredName }));
-  });
-});
+
 //***********************************SOCKET.IO******************************************************/
 gameEngine.createTestGameInst(
   "user1",
@@ -155,19 +50,21 @@ gameEngine.createTestGameInst(
 io.on("connection", socket => {
   console.log("connected");
   socket.on("get-game-data", message => {
-    socket.emit("game-data", gameEngine.getGameInst(message.gameId));
+    socket.emit("game-data", gameEngine.getGameInst("test"));
   });
   socket.on("game-input", input => {
-    gameEngine.handlerUserInput({
+    console.log("here");
+    let changes = gameEngine.handlerUserInput({
       gameId: "test",
       action: input,
       team: "user1"
     });
     socket.emit("game-state-change", {
       success: true,
-      changes: [input],
+      changes: changes,
       team: "user1"
     });
+    socket.emit("game,data", gameEngine.getGameInst("test"));
   });
 
   console.log(socket.request.headers.cookie);
