c4bac0b1c9abbe571cac656a0699b751d2edd33b
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-16 14:16:12 -0400

    Add 'Leave Lobby' button to Lobby component

--- src/reducer.js
+++ src/reducer.js
@@ -1,42 +1,44 @@
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "logged-in":
-      return {
-        ...state,
-        loggedIn: action.toggle,
-        currentUser: action.username
-      };
-    case "SIGN-OUT":
-      return { ...state, loggedIn: action.toggle, currentUser: "" };
-    case "JOIN-LOBBY":
-      return { ...state, currentLobby: action.lobbyId, inLobby: true };
-    case "show-message":
-      return { ...state, message: action.message };
-    case "set-action-menu":
-      return {
-        ...state,
-        actionMenu: {
-          visible: action.visible,
-          xPos: action.xPos,
-          yPos: action.yPos,
-          options: action.options
-        }
-      };
-    case "set-game-data":
-      return {
-        ...state,
-        gameData: {
-          ...action.data
-        }
-      };
-    case "set-game-state":
-      return {
-        ...state,
-        gameState: action.gameState
-      };
-    default:
-      return state;
-  }
+   switch (action.type) {
+      case "logged-in":
+         return {
+            ...state,
+            loggedIn: action.toggle,
+            currentUser: action.username
+         };
+      case "SIGN-OUT":
+         return { ...state, loggedIn: action.toggle, currentUser: "" };
+      case "JOIN-LOBBY":
+         return { ...state, currentLobbyId: action.lobbyId, inLobby: true };
+      case "LEAVE-LOBBY":
+         return { ...state, currentLobbyId: "", inLobby: false }
+      case "show-message":
+         return { ...state, message: action.message };
+      case "set-action-menu":
+         return {
+            ...state,
+            actionMenu: {
+               visible: action.visible,
+               xPos: action.xPos,
+               yPos: action.yPos,
+               options: action.options
+            }
+         };
+      case "set-game-data":
+         return {
+            ...state,
+            gameData: {
+               ...action.data
+            }
+         };
+      case "set-game-state":
+         return {
+            ...state,
+            gameState: action.gameState
+         };
+      default:
+         return state;
+   }
 };
 
 export default reducer;
--- src/store.js
+++ src/store.js
@@ -2,31 +2,31 @@ import { createStore } from "redux";
 import reducer from "./reducer.js";
 
 const initialState = {
-  loggedIn: false,
-  currentUser: "",
-  inLobby: false,
-  currentLobby: "",
-  message: "Welcome!",
-  actionMenu: {
-    visible: false,
-    xPos: 0,
-    yPos: 0,
-    options: []
-  },
-  gameData: {
-    actors: [],
-    width: 0,
-    height: 0
-  },
-  gameState: {
-    type: "select-unit"
-  }
+   loggedIn: false,
+   currentUser: "",
+   inLobby: false,
+   currentLobbyId: "",
+   message: "Welcome!",
+   actionMenu: {
+      visible: false,
+      xPos: 0,
+      yPos: 0,
+      options: []
+   },
+   gameData: {
+      actors: [],
+      width: 0,
+      height: 0
+   },
+   gameState: {
+      type: "select-unit"
+   }
 };
 
 const store = createStore(
-  reducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   reducer,
+   initialState,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
