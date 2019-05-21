45c0fc9b64991be1273bbc6ab7a8d1272740f35d
Kenton <kenrilea@gmail.com>
2019-05-21 14:30:50 -0400

    games now update the leaderboard

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ app.post("/signup", upload.none(), function(req, res) {
         wins: 0,
         losses: 0,
         points: 0,
+        isAdmin: false,
         currentLobby: "",
         profilePic: "/assets/default-user.jpg",
         status: "playing Super Chess II",
@@ -990,6 +991,66 @@ io.on("connection", socket => {
           action: input,
           team: result[0].user
         });
+        let isGameOver = gameEngine.checkGameOver(gameId);
+        if (isGameOver.success === false) {
+          console.log("game in session");
+        }
+        if (isGameOver.success === true) {
+          let win = [0, 0];
+          let loss = [0, 0];
+          isGameOver.players.forEach((player, index) => {
+            if (isGameOver.winner === player) {
+              win[index] = 1;
+            }
+            if (isGameOver.winner !== player) {
+              loss[index] = 1;
+            }
+            delete UserGameAssoc[player];
+          });
+          usersCollection
+            .find({ username: isGameOver.players[0] })
+            .toArray((err, result) => {
+              console.log(result[0]);
+              let newUserData = result[0];
+              newUserData = {
+                ...newUserData,
+                wins: newUserData.wins + win[0],
+                losses: newUserData.losses + loss[0]
+              };
+              usersCollection.updateOne(
+                { _id: result[0]._id },
+                {
+                  $set: {
+                    wins: newUserData.wins + win[0],
+                    losses: newUserData.losses + loss[0]
+                  }
+                },
+                (err, result) => {
+                  if (err) throw err;
+                  console.log(`DB: editing user information: ${"username"}`);
+                }
+              );
+            });
+          usersCollection
+            .find({ username: isGameOver.players[1] })
+            .toArray((err, result) => {
+              console.log(result[0]);
+              let newUserData = result[0];
+              usersCollection.updateOne(
+                { _id: result[0]._id },
+                {
+                  $set: {
+                    wins: newUserData.wins + win[1],
+                    losses: newUserData.losses + loss[1]
+                  }
+                },
+                (err, result) => {
+                  if (err) throw err;
+                  console.log(`DB: editing user information: ${"username"}`);
+                }
+              );
+            });
+        }
         io.in(gameId).emit("game-state-change", changes);
       });
   });
