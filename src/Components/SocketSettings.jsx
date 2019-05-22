140517d69d9d802498ce3d742721e2744cf121c5
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-22 12:12:57 -0400

    CSS throughtout, Lobby checks that user is logged in

--- src/Components/SocketSettings.jsx
+++ src/Components/SocketSettings.jsx
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import { Manager } from "socket.io-client";
 
 //USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON 
-const socket = io("http://192.168.2.127:4000", {
+const socket = io("http://localhost:4000", {
    autoConnect: false
 });
 
