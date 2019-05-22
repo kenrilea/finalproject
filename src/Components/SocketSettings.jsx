0a44a77836796ed8cdf1765cd9bd6ec7724163e8
Kenton <kenrilea@gmail.com>
2019-05-22 10:52:44 -0400

    leaderboard new rank names

--- src/Components/SocketSettings.jsx
+++ src/Components/SocketSettings.jsx
@@ -1,9 +1,9 @@
 import io from "socket.io-client";
 import { Manager } from "socket.io-client";
 
-//USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON 
-const socket = io("http://192.168.2.127:4000", {
-   autoConnect: false
+//USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON
+const socket = io("http://localhost:4000", {
+  autoConnect: false
 });
 
 export default socket;
