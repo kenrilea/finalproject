5d566e9646f4f090893f770b266b98a2c7af41da
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-22 19:00:36 -0400

    Socket open and close on autologin

--- src/Components/AutoLogin.jsx
+++ src/Components/AutoLogin.jsx
@@ -1,41 +1,43 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+import socket from "./SocketSettings.jsx";
 
 class UnconnectedAutoLogin extends Component {
-  componentDidMount = () => {
-    //If not loggedIn in props, check if the cookie is valid!
-    if (!this.props.loggedIn) {
-      fetch("/verify-cookie", { credentials: "include" })
-        .then(res => {
-          return res.text();
-        })
-        .then(resBody => {
-          let parsedBody = JSON.parse(resBody);
-          if (!parsedBody.success) {
-            console.log("Auto-Login DENIED - Invalid cookie");
-            return;
-          }
+   componentDidMount = () => {
+      //If not loggedIn in props, check if the cookie is valid!
+      if (!this.props.loggedIn) {
+         fetch("/verify-cookie", { credentials: "include" })
+            .then(res => {
+               return res.text();
+            })
+            .then(resBody => {
+               let parsedBody = JSON.parse(resBody);
+               if (!parsedBody.success) {
+                  console.log("Auto-Login DENIED - Invalid cookie");
+                  return;
+               }
+               console.log(`Automatically logging in ${parsedBody.username}`);
+               console.log(parsedBody);
+               this.props.dispatch({
+                  type: "logged-in",
+                  toggle: true,
+                  username: parsedBody.username
+               });
+               socket.close()
+               socket.open()
+            });
+      }
+   };
 
-          console.log(`Automatically logging in ${parsedBody.username}`);
-          console.log(parsedBody);
-          this.props.dispatch({
-            type: "logged-in",
-            toggle: true,
-            username: parsedBody.username
-          });
-        });
-    }
-  };
-
-  render = () => {
-    return null;
-  };
+   render = () => {
+      return null;
+   };
 }
 
 let mapStateToProps = state => {
-  return {
-    loggedIn: state.loggedIn
-  };
+   return {
+      loggedIn: state.loggedIn
+   };
 };
 
 let AutoLogin = connect(mapStateToProps)(UnconnectedAutoLogin);
