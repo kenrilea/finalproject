5ee2e265e45a2b471c93653e3ad78336d2d3dcd0
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-22 13:05:40 -0400

    Fix button zindex

--- src/Components/CloudsBG.jsx
+++ src/Components/CloudsBG.jsx
@@ -3,79 +3,79 @@ import React, { Component } from "react";
 import "../css/cloudsBG.css";
 
 let buttonStyle = {
-  display: "block",
-  position: "absolute",
-  bottom: "-775px",
-  left: "30px",
-  zIndex: "999",
-  width: "145px",
-  minWidth: "145px",
-  maxWidth: "225px"
+   display: "block",
+   position: "fixed",
+   bottom: "30px",
+   left: "30px",
+   zIndex: "1005",
+   width: "145px",
+   minWidth: "145px",
+   maxWidth: "225px"
 };
 
 class CloudsBG extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      style: {
-        display: "block",
-        text: "Hide Clouds"
+   constructor(props) {
+      super(props);
+      this.state = {
+         style: {
+            display: "block",
+            text: "Hide Clouds"
+         }
+      };
+   }
+
+   handleClouds = () => {
+      if (this.state.style.display === "block") {
+         this.setState({
+            style: {
+               display: "none",
+               text: "Show Clouds"
+            }
+         });
+      } else {
+         this.setState({
+            style: {
+               display: "block",
+               text: "Hide Clouds"
+            }
+         });
       }
-    };
-  }
+   };
 
-  handleClouds = () => {
-    if (this.state.style.display === "block") {
-      this.setState({
-        style: {
-          display: "none",
-          text: "Show Clouds"
-        }
-      });
-    } else {
-      this.setState({
-        style: {
-          display: "block",
-          text: "Hide Clouds"
-        }
-      });
-    }
-  };
+   render = () => {
+      return (
+         <div>
+            <div style={this.state.style} id="background-wrap">
+               <div className="x1">
+                  <div className="cloud" />
+               </div>
 
-  render = () => {
-    return (
-      <div>
-        <div style={this.state.style} id="background-wrap">
-          <div className="x1">
-            <div className="cloud" />
-          </div>
+               <div className="x2">
+                  <div className="cloud" />
+               </div>
 
-          <div className="x2">
-            <div className="cloud" />
-          </div>
+               <div className="x3">
+                  <div className="cloud" />
+               </div>
 
-          <div className="x3">
-            <div className="cloud" />
-          </div>
+               <div className="x4">
+                  <div className="cloud" />
+               </div>
 
-          <div className="x4">
-            <div className="cloud" />
-          </div>
-
-          <div className="x5">
-            <div className="cloud" />
-          </div>
-        </div>
-        <button
-          style={buttonStyle}
-          className="material-button"
-          onClick={this.handleClouds}
-        >
-          {this.state.style.text}
-        </button>
-      </div>
-    );
-  };
+               <div className="x5">
+                  <div className="cloud" />
+               </div>
+            </div>
+            <button
+               style={buttonStyle}
+               className="material-button"
+               onClick={this.handleClouds}
+            >
+               {this.state.style.text}
+            </button>
+         </div>
+      );
+   };
 }
 
 export default CloudsBG;
