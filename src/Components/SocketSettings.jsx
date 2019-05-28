70aa86e6656084785b3882e7dbacd86fab90c049
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-27 23:19:06 -0400

    Socket test

--- src/Components/SocketSettings.jsx
+++ src/Components/SocketSettings.jsx
@@ -2,11 +2,11 @@ import io from "socket.io-client";
 import { Manager } from "socket.io-client";
 
 //USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON
-const socket = io("http://134.209.119.133:4000", {
+const socket = io.connect("http://134.209.119.133:4000", {
    autoConnect: false,
 });
 
-socket.connect("http://134.209.119.133:4000")
+
 
 
 export default socket;
