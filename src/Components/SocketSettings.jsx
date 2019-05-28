d85ffed65467a29c25b865119585a8dce0ffe7f4
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-27 22:23:34 -0400

    Typo in socket IP >.<

--- src/Components/SocketSettings.jsx
+++ src/Components/SocketSettings.jsx
@@ -4,7 +4,7 @@ import { Manager } from "socket.io-client";
 //USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON
 
 //Ryan's droplet IP address
-const socket = io("http://134.209.119.133:4000/:4000", {
+const socket = io("http://134.209.119.133:4000", {
    autoConnect: false,
 });
 
