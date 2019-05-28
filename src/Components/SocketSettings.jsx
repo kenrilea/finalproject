d1f1dafa635ea3a7dbcbe87107a4da763a662da3
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-27 22:39:50 -0400

    Socket test

--- src/Components/SocketSettings.jsx
+++ src/Components/SocketSettings.jsx
@@ -2,9 +2,7 @@ import io from "socket.io-client";
 import { Manager } from "socket.io-client";
 
 //USE IP ADDRESS FOR TESTING ON MOBILE WITH NODEMON
-
-//Ryan's droplet IP address
-const socket = io("http://134.209.119.133:4000", {
+const socket = io("http://0.0.0.0:4000", {
    autoConnect: false,
 });
 
